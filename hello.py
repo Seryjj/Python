962a91a8e4d4de164fed8b709bb111b1d630b462
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2022-11-28 22:17:58 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+
+# a=int(input())
+# print(a**10)
+
+#print('Hello')
+
+# a=int(input('Введите а'))
+# b=int(input('Введите b'))
+
+# a = list(map(int, input()))
+# print(max(a))
+
+
+
+# a = input().split()
+# print(a)
+# max = int(a[0])
+# for i in range(5):
+# if int(a[i]) > max:
+# max = int(a[i])
+# print(max)
+
