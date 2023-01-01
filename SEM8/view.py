aef9032468c0dbca78c2a01cfc0241640cc5fd13
Алексеев Сергей <seryjj.alexeev@yandex.ru>
2023-01-01 23:28:07 +0300

    final

---
+++ SEM8/view.py
@@ -0,0 +1,16 @@
+def contact_to_s():
+    return input('Введите информацию для поиска ')
+
+
+def choose_mode():
+    return input('Введите режим работы(1 - добавление, 2 - поиск) ')
+
+def new_contact():
+    name = input('Введите имя: ')
+    phone_num=input('Введите номер: ')
+    return f'{name} || {phone_num}'
+
+def show_found(result):
+    print('Результат поиска : ')
+    for i in result:
+        print(i)
